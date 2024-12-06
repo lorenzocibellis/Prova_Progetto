af58bc697c32bdfd5916b4dc805a4b978fb885a7
Christian <c.decesare3@studenti.unisa.it>
2024-12-06 15:00:19 +0100

    Classe Contatto aggiunta

---
+++ ProvaC/Prova/src/main/java/Contatto.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Acer
+ */
+public class Contatto {
+    private String nome;
+    private String cognome;
+
+    public Contatto(String nome, String cognome) {
+        this.nome = nome;
+        this.cognome = cognome;
+    }
+    
+    
+}
