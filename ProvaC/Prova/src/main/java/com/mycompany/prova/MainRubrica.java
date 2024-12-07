2c342404dd5f4e71710c3bd280aed8ae29a1cb5d
lorenzocibellis <l.cibellis@studenti.unisa.it>
2024-12-07 17:13:25 +0100

    modifiche generali doxygen

--- ProvaC/Prova/src/main/java/com/mycompany/prova/MainRubrica.java
+++ ProvaC/Prova/src/main/java/com/mycompany/prova/MainRubrica.java
@@ -13,19 +13,19 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
- * @class MainRubrica
+ * @file MainRubrica
  * @brief Classe principale per l'applicazione fatta in JavaFX.
  *
- * Questa classe rappresenta l'ingresso dell'applicazione JavaFX e carica
- * l'interfaccia utente da un file FXML.
+ *        Questa classe rappresenta l'ingresso dell'applicazione JavaFX e carica
+ *        l'interfaccia utente da un file FXML.
  */
 public class MainRubrica extends Application {
 
     /**
      * @brief Metodo principale di avvio dell'applicazione JavaFX.
      *
-     * Questo metodo viene invocato automaticamente dal framework JavaFX 
-     * e si occupa di configurare e mostrare la finestra principale dell'applicazione.
+     *        Questo metodo viene invocato automaticamente dal framework JavaFX 
+     *        e si occupa di configurare e mostrare la finestra principale dell'applicazione.
      * 
      * @param stage La finestra principale dell'applicazione (Stage).
      * @throws Exception Se si verifica un errore durante il caricamento del file FXML.
@@ -45,8 +45,8 @@ public class MainRubrica extends Application {
     /**
      * @brief Metodo main per avviare l'applicazione.
      *
-     * Questo metodo invoca il metodo `launch` di JavaFX per iniziare l'esecuzione
-     * dell'applicazione.
+     *        Questo metodo invoca il metodo `launch` di JavaFX per iniziare l'esecuzione
+     *        dell'applicazione.
      * 
      * @param args Gli argomenti della riga di comando passati all'applicazione.
      */
