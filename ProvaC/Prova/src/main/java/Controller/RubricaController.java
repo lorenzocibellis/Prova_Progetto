1c788bb8e0309c891f8a7066471fd11127038882
Christian <c.decesare3@studenti.unisa.it>
2024-12-07 17:10:07 +0100

    Inseriti i commenti per doxygen in RubricaController e MainRubrica

--- ProvaC/Prova/src/main/java/Controller/RubricaController.java
+++ ProvaC/Prova/src/main/java/Controller/RubricaController.java
@@ -1,10 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+/**
+ * @brief Classe Controller per la gestione dell'interfaccia dell'applicazione "Rubrica".
+ * Questa classe gestisce le interazioni con l'utente e le azioni sulla Rubrica.
  */
 package Controller;
-import GestioneRubrica.Rubrica;
 
+import GestioneRubrica.Rubrica;
 import java.awt.Button;
 import java.awt.TextField;
 import java.awt.event.ActionEvent;
@@ -12,47 +12,108 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableView;
 
-public class RubricaController extends Controller{
+public class RubricaController extends Controller {
 
-    //attributi
+    /**
+     *  Puntatore all'istanza di Rubrica gestita.
+     */
     private Rubrica rubricaPointer;
+
+    /**
+     *  Tabella per visualizzare la lista dei contatti nella Rubrica.
+     */
     private TableView rubricaList;
+
+    /**
+     *  Pulsante per rimuovere un contatto selezionato dalla Rubrica.
+     */
     private Button removeButton;
+
+    /**
+     * Pulsante per aggiungere un nuovo contatto alla Rubrica.
+     */
     private Button addButton;
+
+    /**
+     * Campo di testo per cercare contatti nella Rubrica.
+     */
     private TextField researchField;
+
+    /**
+     *  Pulsante per importare contatti nella Rubrica.
+     */
     private Button importButton;
+
+    /**
+     *  Pulsante per esportare contatti dalla Rubrica.
+     */
     private Button exportButton;
+
+    /**
+     * Pulsante per uscire dall'applicazione.
+     */
     private Button exitButton;
 
-   
-    //metodi
-    private void setRubricaList(Rubrica r){
+    /**
+     * @brief Imposta la tabella con una nuova istanza di Rubrica.
+     * 
+     * @param r la nuova Rubrica da visualizzare nella tabella
+     */
+    private void setRubricaList(Rubrica r) {
     
     }
-    
-    private void add(ActionEvent e){
-    
-    }
-    
-    private void research(ActionEvent e){
-    
-    }
-    
-    private void delete(ActionEvent e){
+
+    /**
+     * @brief Aggiunge un nuovo contatto alla Rubrica.
+     * 
+     * @param e evento che attiva l'aggiunta del contatto
+     */
+    private void add(ActionEvent e) {
     
     }
-    
-    private void openContact(ActionEvent e){
-    
-    }
-    
-    private void exportRubrica(ActionEvent e){
-    
-    }
-    
-    private void importRubrica(ActionEvent e){
+
+    /**
+     * @brief Effettua una ricerca di contatti nella Rubrica.
+     * 
+     * @param e evento che attiva la ricerca
+     */
+    private void research(ActionEvent e) {
     
     }
+
+    /**
+     * @brief Elimina un contatto selezionato dalla Rubrica.
+     * 
+     * @param e evento che attiva l'eliminazione
+     */
+    private void delete(ActionEvent e) {
     
+    }
+
+    /**
+     * @brief Apre i dettagli di un contatto selezionato.
+     * 
+     * @param e evento che attiva l'apertura del contatto
+     */
+    private void openContact(ActionEvent e) {
     
+    }
+
+    /**
+     * @brief Esporta i contatti della Rubrica in un file.
+     * 
+     * @param e evento che attiva l'esportazione
+     */
+    private void exportRubrica(ActionEvent e) {
+    
+    }
+
+    /**
+     * @brief Importa contatti nella Rubrica da un file.
+     * 
+     * @param e evento che attiva l'importazione
+     */
+    private void importRubrica(ActionEvent e) {
+    
+    }
 }
--- ProvaC/Prova/src/main/java/com/mycompany/prova/MainRubrica.java
+++ ProvaC/Prova/src/main/java/com/mycompany/prova/MainRubrica.java
@@ -13,23 +13,42 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
+ * @class MainRubrica
+ * @brief Classe principale per l'applicazione fatta in JavaFX.
  *
- * @author lorenzocibellis
+ * Questa classe rappresenta l'ingresso dell'applicazione JavaFX e carica
+ * l'interfaccia utente da un file FXML.
  */
 public class MainRubrica extends Application {
 
+    /**
+     * @brief Metodo principale di avvio dell'applicazione JavaFX.
+     *
+     * Questo metodo viene invocato automaticamente dal framework JavaFX 
+     * e si occupa di configurare e mostrare la finestra principale dell'applicazione.
+     * 
+     * @param stage La finestra principale dell'applicazione (Stage).
+     * @throws Exception Se si verifica un errore durante il caricamento del file FXML.
+     */
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("file.fxml"));
         
+        // Creazione della scena con il layout caricato
         Scene scene = new Scene(root);
         
+        // Configurazione dello stage principale
         stage.setScene(scene);
         stage.show();
     }
 
     /**
-     * @param args the command line arguments
+     * @brief Metodo main per avviare l'applicazione.
+     *
+     * Questo metodo invoca il metodo `launch` di JavaFX per iniziare l'esecuzione
+     * dell'applicazione.
+     * 
+     * @param args Gli argomenti della riga di comando passati all'applicazione.
      */
     public static void main(String[] args) {
         launch(args);
